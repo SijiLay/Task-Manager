6bf5f5ebe4afea95fc48343de290e80955fe65d7
Olamayowa Layeni <OlasijiLayeni@gmail.com>
2026-06-07 13:23:19 -0400

    Implemented core task manager functionality

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,69 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    TaskManager manager = new TaskManager();
+    Scanner scanner = new Scanner(System.in);
+    private boolean programRunning = false;
+
     public static void main(String[] args){
-        TaskManager manager = new TaskManager();
-        System.out.println(manager.hasTask());
-        manager.addTask("Dishes");
-        System.out.println(manager.hasTask());
-        System.out.println(manager.getTaskByNumber(1));
-        manager.completeTask(1);
-        System.out.println(manager.getTaskByNumber(1));
-        manager.addTask("Clothes");
-        manager.displayTaskList();
-        manager.deleteTask(1);
-        System.out.println(manager.hasTask());
+        Main app = new Main();
+        app.run();
+    }
+    public void run(){
+        programRunning=true;
+        while(programRunning){
+            showMenu();
+            int choice=scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("You picked: " + choice);
+            handleChoice(choice);
+
+        }
+    }
+
+    public void showMenu(){
+        System.out.println("1. Add Task");
+        System.out.println("2. View Tasks");
+        System.out.println("3. Complete Task");
+        System.out.println("4. Delete Task");
+        System.out.println("5. Exit program");
+    }
+
+    public String getTaskName(){
+        System.out.println("Enter task name: ");
+        return scanner.nextLine();
+    }
+
+    public int getTaskNumber(){
+        System.out.println("Enter task number: ");
+        return scanner.nextInt();
+    }
+
+    public void handleChoice(int choice){
+        if(choice==1){
+            String taskName=getTaskName();
+            manager.addTask(taskName);
+            System.out.println("Success");
+        }
+        else if(choice==2){
+            manager.displayTaskList();
+        }
+        else if(choice==3){
+            int taskNum=getTaskNumber();
+            manager.completeTask(taskNum);
+            //display message
+        }
+        else if(choice==4){
+            int taskNum=getTaskNumber();
+            manager.deleteTask(taskNum);
+        }
+        else if(choice ==5){
+            programRunning=false;
+        }
+        else {
+            System.out.println("invalid choice option");
+        }
 
     }
 }
